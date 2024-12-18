a50ca946a5942282f9dfc175cc54c5b3da861738
collins-kimotho <collinskimotho16@gmail.com>
2024-12-18 05:41:21 +0300

    wait for elements to appear

---
+++ footballMatches/matches.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+
+
+
+url = 'https://www.adamchoi.co.uk/overs/detailed'
+path = '/home/optimus/Desktop/chromedriver'
+
+service = Service(path)
+driver = webdriver.Chrome(service=service)
+
+driver.get(url)
+
+# time.sleep(5)
+
+
+try:
+    dropdown = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.ID, "country"))
+    )
+    select = Select(dropdown)
+    select.select_by_visible_text('Spain')
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+
+time.sleep(5)
+
+driver.quit()
