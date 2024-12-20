8b108b1298711e9d328b778ecfe301ade33b4be2
collins-kimotho <collinskimotho16@gmail.com>
2024-12-20 13:57:14 +0300

    add data to mongo

--- quotesSpider/quotesbot/quotesbot/pipelines.py
+++ quotesSpider/quotesbot/quotesbot/pipelines.py
@@ -1,13 +1,48 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
+# import sqlite3
+# import mysql.connector
+import pymongo
 
 class QuotesbotPipeline:
+
+    def __init__(self):
+        # self.create_connection()
+        # self.create_table()
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+        db = self.conn['myquotes']
+        self.collection = db['quotes_tb']
+
+    # def create_connection(self):
+    #     self.conn = mysql.connector.connect(
+    #         host = 'localhost',
+    #         user = 'root',
+    #         passwd = 'Two2061996@',
+    #         database = 'myquotes'
+    #         )
+    #     self.curr = self.conn.cursor()
+
+    # def create_table(self):
+    #     self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+    #     self.curr.execute("""
+    #         CREATE TABLE quotes_tb(
+    #             title TEXT,
+    #             author TEXT,
+    #             tag TEXT
+    #         )
+    #     """)
+
     def process_item(self, item, spider):
+        # self.store_db(item)
+        # return item
+        self.collection.insert_one(dict(item))
         return item
+
+    # def store_db(self, item):
+    #     self.curr.execute("""INSERT INTO quotes_tb VALUES (%s, %s, %s)""", (
+    #         item['title'][0],
+    #         item['author'][0],
+    #         item['tag'][0]
+    #     ))
+    #     self.conn.commit()
