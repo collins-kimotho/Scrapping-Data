1d2034235769cb753063dfb3367704eb9713e81c
collins-kimotho <collinskimotho16@gmail.com>
2024-12-20 20:29:58 +0300

    handling login

--- quotesSpider/quotesbot/quotesbot/spiders/main.py
+++ quotesSpider/quotesbot/quotesbot/spiders/main.py
@@ -1,22 +1,28 @@
 import scrapy
-
+from scrapy.http import FormRequest
 from ..items import QuotesbotItem
 
 class QuoteScrape(scrapy.Spider):
     name = 'quotes'
     start_urls = [
-        'https://quotes.toscrape.com/'
+        'https://quotes.toscrape.com/login'
     ]
 
     def parse(self, response):
+        token = response.css('form input::attr(value)').extract_first()
+        return FormRequest.from_response(response, formdata={
+            'csrf_token': token,
+            'username': 'loginName',
+            'password': 'loginPassword'
+        }, callback=self.start_scraping)
 
+    def start_scraping(self, response):
         items = QuotesbotItem()
-
         all_quotes = response.css('div.quote')
 
         for quote in all_quotes:
             title = quote.css('span.text::text').extract()
-            author = quote.css('small.author::text').extract()
+            author = quote.css('.author::text').extract()
             tag = quote.css('.tag::text').extract()
 
             items['title'] = title
@@ -24,9 +30,3 @@ class QuoteScrape(scrapy.Spider):
             items['tag'] = tag
 
             yield items
-
-        # Pagination
-        next_page = response.css('li.text a::attr(href)').get()
-
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
