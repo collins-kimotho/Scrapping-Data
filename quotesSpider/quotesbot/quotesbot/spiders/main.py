233b8b80dc1d5ab99adcfb7e6705d62250524db9
collins-kimotho <collinskimotho16@gmail.com>
2024-12-20 08:38:27 +0300

    item containers

--- quotesSpider/quotesbot/quotesbot/spiders/main.py
+++ quotesSpider/quotesbot/quotesbot/spiders/main.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from ..items import QuotesbotItem
+
 class QuoteScrape(scrapy.Spider):
     name = 'quotes'
     start_urls = [
@@ -7,14 +9,18 @@ class QuoteScrape(scrapy.Spider):
     ]
 
     def parse(self, response):
+
+        items = QuotesbotItem()
+
         all_quotes = response.css('div.quote')
 
         for quote in all_quotes:
             title = quote.css('span.text::text').extract()
             author = quote.css('small.author::text').extract()
             tag = quote.css('.tag::text').extract()
-            yield{
-                'Title': title,
-                'Author': author,
-                'Tags': tag            
-            }
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
