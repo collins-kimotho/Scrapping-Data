336734d8a34ce577b15bf761576666de970ea213
collins-kimotho <collinskimotho16@gmail.com>
2024-12-22 11:26:40 +0300

    get title and author

--- audibleSpider/audibleSpider/audibleSpider/spiders/audible.py
+++ audibleSpider/audibleSpider/audibleSpider/spiders/audible.py
@@ -7,4 +7,8 @@ class AudibleSpider(scrapy.Spider):
     start_urls = ["https://audible.com/search"]
 
     def parse(self, response):
-        pass
+        container = response.xpath('//div[@class="adbl-impression-container "]/div/span/ul/li')
+
+        for product in container:
+            p_title = response.xpath('//h3[contains(@class, "bc-heading ")]/a/text()').getall()
+            p_author = response.xpath('//li[contains(@class, "authorLabel")]/span/a/text()').ggetall()
