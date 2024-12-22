6a0094ff084e6b130423dcdec3bc314ea05335d0
collins-kimotho <collinskimotho16@gmail.com>
2024-12-22 12:14:21 +0300

    fixed title

--- audibleSpider/audibleSpider/audibleSpider/spiders/audible.py
+++ audibleSpider/audibleSpider/audibleSpider/spiders/audible.py
@@ -7,8 +7,24 @@ class AudibleSpider(scrapy.Spider):
     start_urls = ["https://audible.com/search"]
 
     def parse(self, response):
-        container = response.xpath('//div[@class="adbl-impression-container "]/div/span/ul/li')
+        # container = response.xpath('//div[@class="adbl-impression-container "]/div/span/ul/li')
+        container = response.xpath('//div[@class="adbl-impression-container "]//li[contains(@class, "productListItem")]')
 
         for product in container:
-            p_title = response.xpath('//h3[contains(@class, "bc-heading ")]/a/text()').getall()
-            p_author = response.xpath('//li[contains(@class, "authorLabel")]/span/a/text()').ggetall()
+            p_title = product.xpath('.//h3[contains(@class, "bc-heading")]/a/text()').getall()
+            p_author = product.xpath('.//li[contains(@class, "authorLabel")]/span/a/text()').getall()
+            p_length = product.xpath('.//li[contains(@class, "runtimeLabel")]/span/text()').get()
+
+            yield {
+                'Title': p_title,
+                "Author": p_author,
+                "Length": p_length
+            }
+
+        pagination = response.xpath('//ul[contains(@class, "pagingElements")]')
+        next_page_url = pagination.xpath('.//span[contains(class, "nextButton")]/a/@href').get()
+        button_disabled = pagination.xpath('.//span[contains(@class , "nextButton")]/a/@aria-disabled').get()
+    
+
+        if next_page_url and button_disabled:
+            yield response.follow(url=next_page_url, callback=self.parse)
---
+++ audibleSpider/audibleSpider/audibleSpider/spiders/shell.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scrapy shell https://www.audible.com/
